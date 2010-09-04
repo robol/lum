54c85aa8c47b32b96dff6aa8251581ab77fff3f3
Leonardo Robol <leo@robol.it>
2010-09-04 15:02:38 +0200

    Connessione di base funzionante

--- lum/exceptions.py
+++ lum/exceptions.py
@@ -1,13 +1,13 @@
 #
 # -*- coding: utf-8 -*-
 
-class LumException(Exception):
+class LumError(Exception):
 
     def __init__(self, message):
         self._message = message
 
     def __repr__(self):
-        return "<LumException: %s" % self._message
+        return "<LumError: %s>" % self._message
 
     def __str__(self):
         return self.__repr__(self)
---
+++ lum/ldapProtocol.py
@@ -0,0 +1,121 @@
+# -*- coding: utf-8 -*-
+#
+#
+
+import ldap, ldap.modlist, re, ldif, sys
+from exceptions import LumError
+
+ldifwriter = ldif.LDIFWriter(sys.stdout)
+
+class UserModel():
+
+    def __init__(self, username, given_name, sn, uid, gid,
+                 login_shell = "/bin/sh", additional_data = None):
+
+        self.__ldif = dict ()
+        self.__ldif['objectClass'] = ["inetOrgPerson",
+                                      "posixAccount",
+                                      "person",
+                                      "shadowAccount",
+                                      "organizationalPerson",
+                                      "top"]
+        
+        self.__ldif['uid'] = [str(username)]
+        self.__ldif['cn'] = [str(username)]
+        self.__ldif['sn'] = [str(sn)]
+        self.__ldif['gecos'] = [" ".join([given_name, sn])]
+        self.__ldif['givenName'] = [str(given_name)]
+        self.__ldif['uidNumber'] = [str(uid)]
+        self.__ldif['gidNumber'] = [str(gid)]
+        self.__ldif['loginShell'] = [str(login_shell)]
+        self.__ldif['homeDirectory'] = ["/home/%s" % username]
+
+        if additional_data is None:
+            return
+        for key in additional_data:
+            self.__ldif[key] = list(str(additional_data[key]))
+
+    def __getitem__(self, item):
+        return self.__ldif[item]
+
+    def to_ldif(self):
+        return dict(self.__ldif)
+
+class Connection():
+
+    def __init__(self, config, password = None):
+        """
+        Create a connection to the specified uri
+        """
+
+        # Piccola funzioncina comoda per ottenere dati
+        # dalla configurazione
+        get = lambda x : config.get("LDAP", x)
+        self.__get = get
+
+        self.__config = config
+        self.__ldap = ldap.initialize(get("uri"))
+
+        bind_dn = get("bind_dn")
+        self.__base_dn = get("base_dn")
+
+        # Retrieve password from the file specified in
+        # the configuration file
+        try:
+            if password is None:
+                with open(get("secret_file"), 'r') as f:
+                    password = f.read().strip("\n")
+        except IOError, e:
+            raise LumError('Error opening password file %s' % get("secret_file"))
+
+        # Bind to the database with the provided credentials
+        try:
+            self.__ldap.simple_bind_s(bind_dn, password)
+        except Exception, e:
+            raise LumError('Error connecting to the server, check your credentials')
+
+	if not self.is_present(get("users_ou")):
+		self.add_ou(get("users_ou"))
+
+    def add_user(self, user):
+        """
+        Add a user to the LDAP database
+        """
+        users_ou = self.__get("users_ou")
+        if not self.is_present(users_ou):
+            raise LumError("users organizationalUnit not present, maybe you need to create it?")
+        dn = "uid=%s,%s" % (user['uid'], users_ou)
+        self.__ldap.add_s(dn, ldap.modlist.addModlist(user.to_ldif()))
+
+    def is_present(self, object):
+        """
+        Test if object is present in database
+        """
+	# Strip base_dn
+	object = object.strip(self.__base_dn).strip(",")
+        res = self.__ldap.search_s(self.__base_dn, ldap.SCOPE_SUBTREE, object)
+        if len(res) == 0:
+            return False
+	return True
+        
+    def add_ou(self, ou):
+        """
+        Add an organizationalUnit to the database.
+        Usage example: add_ou("People")
+        """
+        ldif = dict()
+        ldif['objectClass'] = ['organizationalUnit',
+                               'top']
+        ldif['ou'] = [re.findall(r"ou=(\w+),", ou)[0]]
+
+        print "LDIWriter output"
+        ldifwriter.unparse(ou, ldif)
+        print "self.__ldap.add_s(%s, ...)" % ou
+        self.__ldap.add_s(ou, ldap.modlist.addModlist(ldif))
+
+    def get_user(self, key = None):
+        """
+        Get user that match the given key or all users if
+        key is not given.
+        """
+        pass
